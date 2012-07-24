419ff6832d6397e9629aa9187d02610c40ccc3aa
Gabor Papp <gabor.papp@gmail.com>
2012-07-24 23:41:12 +0200

    initial scene node

---
+++ src/Node.cpp
@@ -0,0 +1,183 @@
+#include "Node.h"
+
+using namespace ci;
+using namespace std;
+
+namespace mndl {
+
+Node::Node() :
+	mScale( Vec3f::one() )
+{
+}
+
+Node::Node( const std::string &name ) :
+	mName( name ),
+	mScale( Vec3f::one() )
+{
+}
+
+void Node::setParent( NodeRef parent )
+{
+	mParent = parent;
+}
+
+void Node::setOrientation( const ci::Quatf &q )
+{
+	mOrientation = q;
+	mOrientation.normalize();
+}
+
+const Quatf &Node::getOrientation() const
+{
+	return mOrientation;
+}
+
+void Node::setPosition( const ci::Vec3f &pos )
+{
+	mPosition = pos;
+}
+
+const Vec3f& Node::getPosition() const
+{
+	return mPosition;
+}
+
+void Node::setScale( const Vec3f &scale )
+{
+	mScale = scale;
+}
+
+const Vec3f &Node::getScale() const
+{
+	return mScale;
+}
+
+void Node::setInheritOrientation( bool inherit )
+{
+	mInheritOrientation = inherit;
+}
+
+bool Node::getInheritOrientation() const
+{
+	return mInheritOrientation;
+}
+
+void Node::setInheritScale( bool inherit )
+{
+	mInheritScale = inherit;
+}
+
+bool Node::getInheritScale() const
+{
+	return mInheritScale;
+}
+
+const string &Node::getName() const
+{
+	return mName;
+}
+
+void Node::setInitialState()
+{
+	mInitialPosition = mPosition;
+	mInitialOrientation = mOrientation;
+	mInitialScale = mScale;
+}
+
+void Node::resetToInitialState()
+{
+	mPosition = mInitialPosition;
+	mOrientation = mInitialOrientation;
+	mScale = mInitialScale;
+}
+
+const Vec3f &Node::getInitialPosition() const
+{
+	return mInitialPosition;
+}
+
+const Quatf &Node::getInitialOrientation() const
+{
+	return mInitialOrientation;
+}
+
+const Vec3f &Node::getInitialScale() const
+{
+	return mInitialScale;
+}
+
+const Quatf &Node::getDerivedOrientation() const
+{
+    // TODO: cache transforms, only update when necessary
+	update();
+	return mDerivedOrientation;
+}
+
+const Vec3f &Node::getDerivedPosition() const
+{
+    // TODO: cache transforms, only update when necessary
+	update();
+	return mDerivedPosition;
+}
+
+const Vec3f &Node::getDerivedScale() const
+{
+    // TODO: cache transforms, only update when necessary
+	update();
+	return mDerivedScale;
+}
+
+const Matrix44f &Node::getDerivedTransform() const
+{
+    update();
+
+    mDerivedTransform = Matrix44f::createScale( mDerivedScale );
+    mDerivedTransform *= mDerivedOrientation.toMatrix44();
+    mDerivedTransform.setTranslate( mDerivedPosition );
+
+    return mDerivedTransform;
+}
+
+void Node::update() const
+{
+    if ( mParent )
+    {
+        // update orientation
+        const Quatf &parentOrientation = mParent->getDerivedOrientation();
+        if ( mInheritOrientation )
+        {
+            // Combine orientation with that of parent
+            mDerivedOrientation = getOrientation() * parentOrientation;
+        }
+        else
+        {
+            mDerivedOrientation = getOrientation();
+        }
+
+        // update scale
+        const Vec3f &parentScale = mParent->getDerivedScale();
+        if ( mInheritScale )
+        {
+            mDerivedScale = parentScale * getScale();
+        }
+        else
+        {
+            mDerivedScale = getScale();
+        }
+
+		// change position vector based on parent's orientation & scale
+        mDerivedPosition = ( parentScale * getPosition() ) * parentOrientation;
+
+        // add altered position vector to parent's
+        mDerivedPosition += mParent->getDerivedPosition();
+    }
+    else
+    {
+        // root node, no parent
+        mDerivedOrientation = getOrientation();
+        mDerivedPosition = getPosition();
+        mDerivedScale = getScale();
+    }
+}
+
+} // namespace mndl
---
+++ src/Node.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "cinder/Cinder.h"
+#include "cinder/Vector.h"
+#include "cinder/Quaternion.h"
+#include "cinder/Matrix.h"
+
+namespace mndl {
+
+class Node;
+
+typedef std::shared_ptr< Node > NodeRef;
+
+class Node
+{
+	public:
+		Node();
+		Node( const std::string &name );
+
+		void setParent( NodeRef parent );
+
+		void setOrientation( const ci::Quatf &q );
+		const ci::Quatf &getOrientation() const;
+
+		void setPosition( const ci::Vec3f &pos );
+		const ci::Vec3f& getPosition() const;
+
+		void setScale( const ci::Vec3f &scale );
+		const ci::Vec3f &getScale() const;
+
+		void setInheritOrientation( bool inherit );
+		bool getInheritOrientation() const;
+
+		void setInheritScale( bool inherit );
+		bool getInheritScale() const;
+
+		const std::string &getName() const;
+
+		void setInitialState();
+		void resetToInitialState();
+		const ci::Vec3f &getInitialPosition() const;
+		const ci::Quatf &getInitialOrientation() const;
+		const ci::Vec3f &getInitialScale() const;
+
+		const ci::Quatf &getDerivedOrientation() const;
+		const ci::Vec3f &getDerivedPosition() const;
+		const ci::Vec3f &getDerivedScale() const;
+
+		const ci::Matrix44f &getDerivedTransform() const;
+
+	protected:
+		/// Shared pointer to parent node.
+		NodeRef mParent;
+
+		/// Name of this node.
+		std::string mName;
+
+		/// The orientation of the node relative to its parent.
+		ci::Quatf mOrientation;
+
+		/// The position of the node relative to its parent.
+		ci::Vec3f mPosition;
+
+		/// Scaling factor applied to this node.
+		ci::Vec3f mScale;
+
+		/// Stores whether this node inherits orientation from its parent.
+		bool mInheritOrientation;
+
+		/// Stores whether this node inherits scale from its parent
+		bool mInheritScale;
+
+		/** Cached combined orientation.
+		  This member is the orientation derived by combining the
+		  local transformations and those of it's parents.
+		  */
+		mutable ci::Quatf mDerivedOrientation;
+
+		/** Cached combined position.
+		  This member is the position derived by combining the
+		  local transformations and those of it's parents.
+		  */
+		mutable ci::Vec3f mDerivedPosition;
+
+		/** Cached combined scale.
+		  This member is the position derived by combining the
+		  local transformations and those of it's parents.
+		  */
+		mutable ci::Vec3f mDerivedScale;
+
+		/// The position to use as a base for keyframe animation.
+		ci::Vec3f mInitialPosition;
+		/// The orientation to use as a base for keyframe animation.
+		ci::Quatf mInitialOrientation;
+		/// The scale to use as a base for keyframe animation.
+		ci::Vec3f mInitialScale;
+
+		/// Cached derived transform as a 4x4 matrix
+		mutable ci::Matrix44f mDerivedTransform;
+
+		void update() const;
+};
+
+} // namespace mndl
+
